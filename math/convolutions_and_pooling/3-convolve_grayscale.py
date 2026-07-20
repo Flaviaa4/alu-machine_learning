55045605d3d694bea167108106a924af24bb00dd
Flaviaa4 <f.benihirwe@alustudent.com>
2026-07-20 10:22:50 +0200

    Modified 3rd&4th file

--- math/convolutions_and_pooling/3-convolve_grayscale.py
+++ math/convolutions_and_pooling/3-convolve_grayscale.py
@@ -1,62 +1,42 @@
 #!/usr/bin/env python3
-"""Performs a convolution on images with channels."""
+"""Performs a convolution on grayscale images."""
 
 import numpy as np
 
 
-def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
-    """Performs a convolution on images with channels."""
-    m, h, w, c = images.shape
-    kh, kw, _ = kernel.shape
+def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
+    """Performs a convolution on grayscale images."""
+    m, h, w = images.shape
+    kh, kw = kernel.shape
     sh, sw = stride
 
     if padding == 'same':
-        ph = max((h - 1) * sh + kh - h, 0)
-        pw = max((w - 1) * sw + kw - w, 0)
-
-        ph_top = ph // 2
-        ph_bottom = ph - ph_top
-        pw_left = pw // 2
-        pw_right = pw - pw_left
+        ph = int(np.ceil(((h - 1) * sh + kh - h) / 2))
+        pw = int(np.ceil(((w - 1) * sw + kw - w) / 2))
+        padding = (ph, pw)
 
     elif padding == 'valid':
-        ph_top = 0
-        ph_bottom = 0
-        pw_left = 0
-        pw_right = 0
+        padding = (0, 0)
 
-    else:
-        ph_top = padding[0]
-        ph_bottom = padding[0]
-        pw_left = padding[1]
-        pw_right = padding[1]
+    ph, pw = padding
 
     padded = np.pad(
         images,
-        (
-            (0, 0),
-            (ph_top, ph_bottom),
-            (pw_left, pw_right),
-            (0, 0)
-        ),
+        ((0, 0), (ph, ph), (pw, pw)),
         mode='constant'
     )
 
-    oh = (h + ph_top + ph_bottom - kh) // sh + 1
-    ow = (w + pw_left + pw_right - kw) // sw + 1
+    oh = (h + 2 * ph - kh) // sh + 1
+    ow = (w + 2 * pw - kw) // sw + 1
 
     output = np.zeros((m, oh, ow))
 
     for i in range(oh):
         for j in range(ow):
             output[:, i, j] = np.sum(
-                padded[
-                    :,
-                    i * sh:i * sh + kh,
-                    j * sw:j * sw + kw,
-                    :
-                ] * kernel,
-                axis=(1, 2, 3)
+                padded[:, i * sh:i * sh + kh,
+                       j * sw:j * sw + kw] * kernel,
+                axis=(1, 2)
             )
 
     return output
--- math/convolutions_and_pooling/4-convolve_channels.py
+++ math/convolutions_and_pooling/4-convolve_channels.py
@@ -13,20 +13,32 @@ def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
     if padding == 'same':
         ph = max((h - 1) * sh + kh - h, 0)
         pw = max((w - 1) * sw + kw - w, 0)
+
         ph_top = ph // 2
         ph_bottom = ph - ph_top
         pw_left = pw // 2
         pw_right = pw - pw_left
+
     elif padding == 'valid':
-        ph_top = ph_bottom = pw_left = pw_right = 0
+        ph_top = 0
+        ph_bottom = 0
+        pw_left = 0
+        pw_right = 0
+
     else:
-        ph_top, pw_left = padding
-        ph_bottom = ph_top
-        pw_right = pw_left
+        ph_top = padding[0]
+        ph_bottom = padding[0]
+        pw_left = padding[1]
+        pw_right = padding[1]
 
     padded = np.pad(
         images,
-        ((0, 0), (ph_top, ph_bottom), (pw_left, pw_right), (0, 0)),
+        (
+            (0, 0),
+            (ph_top, ph_bottom),
+            (pw_left, pw_right),
+            (0, 0)
+        ),
         mode='constant'
     )
 
@@ -38,8 +50,12 @@ def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
     for i in range(oh):
         for j in range(ow):
             output[:, i, j] = np.sum(
-                padded[:, i * sh:i * sh + kh,
-                       j * sw:j * sw + kw, :] * kernel,
+                padded[
+                    :,
+                    i * sh:i * sh + kh,
+                    j * sw:j * sw + kw,
+                    :
+                ] * kernel,
                 axis=(1, 2, 3)
             )
 
