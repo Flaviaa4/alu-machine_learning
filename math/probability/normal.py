c3dc7d027d1ff0c8b32de310b118d0943244ebf4
Flaviaa4 <f.benihirwe@alustudent.com>
2026-07-21 12:26:28 +0200

    modified two file

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -3,6 +3,8 @@
 
 
 
+
+
 class Normal:
     """Represents a normal distribution."""
 
--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Poisson distribution module."""
 
+
 class Poisson:
     """Represents a Poisson distribution."""
 
