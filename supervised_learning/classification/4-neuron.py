b6e130603e023e43670ce4eca2ef7da71d76d503
Flaviaa4 <f.benihirwe@alustudent.com>
2026-08-31 14:24:20 +0200

    Created supervised_learning directory and it's first six files

---
+++ supervised_learning/classification/4-neuron.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+"""Defines a single neuron performing binary classification"""
+
+import numpy as np
+
+
+class Neuron:
+    """Class that defines a single neuron performing binary classification"""
+
+    def __init__(self, nx):
+        """Class constructor
+
+        nx is the number of input features to the neuron
+        """
+        if type(nx) is not int:
+            raise TypeError("nx must be a integer")
+        if nx < 1:
+            raise ValueError("nx must be positive")
+
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """Getter for the weights vector"""
+        return self.__W
+
+    @property
+    def b(self):
+        """Getter for the bias"""
+        return self.__b
+
+    @property
+    def A(self):
+        """Getter for the activated output"""
+        return self.__A
+
+    def forward_prop(self, X):
+        """Calculates the forward propagation of the neuron
+
+        X is a numpy.ndarray with shape (nx, m) that contains the input
+        data
+        """
+        Z = np.matmul(self.__W, X) + self.__b
+        self.__A = 1 / (1 + np.exp(-Z))
+        return self.__A
+
+    def cost(self, Y, A):
+        """Calculates the cost of the model using logistic regression
+
+        Y is a numpy.ndarray with shape (1, m) that contains the correct
+        labels for the input data
+        A is a numpy.ndarray with shape (1, m) containing the activated
+        output of the neuron for each example
+        """
+        m = Y.shape[1]
+        cost = -(1 / m) * np.sum(
+            Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A))
+        return cost
+
+    def evaluate(self, X, Y):
+        """Evaluates the neuron's predictions
+
+        X is a numpy.ndarray with shape (nx, m) that contains the input
+        data
+        Y is a numpy.ndarray with shape (1, m) that contains the correct
+        labels for the input data
+        """
+        A = self.forward_prop(X)
+        cost = self.cost(Y, A)
+        prediction = np.where(A >= 0.5, 1, 0)
+        return prediction, cost
