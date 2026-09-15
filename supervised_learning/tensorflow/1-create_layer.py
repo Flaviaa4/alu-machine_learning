76170dcf5083ae5084d1b149fc34ce243087d379
Flaviaa4 <f.benihirwe@alustudent.com>
2026-09-15 10:08:34 +0200

    Modified file 1 of this directory, the Pycodestyle validation was incorrect

--- supervised_learning/tensorflow/1-create_layer.py
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -14,7 +14,10 @@ def create_layer(prev, n, activation):
     """
     initializer = tf.contrib.layers.variance_scaling_initializer(
         mode="FAN_AVG")
-    layer = tf.layers.Dense(units=n, activation=activation,
-                             kernel_initializer=initializer,
-                             name="layer")
+    layer = tf.layers.Dense(
+        units=n,
+        activation=activation,
+        kernel_initializer=initializer,
+        name="layer"
+    )
     return layer(prev)
